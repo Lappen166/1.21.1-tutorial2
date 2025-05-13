b92da7a69df4d2cc1ad15ec486510da9fa9633de
lappen <gruenerpopel1936@gmail.com>
2025-05-13 20:39:08 +0200

    initial

---
+++ src/main/java/com/lappen/tutorialmod/TemplateMod.java
@@ -0,0 +1,17 @@
+package com.lappen.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TemplateMod implements ModInitializer {
+	public static final String MOD_ID = "tutroaial-mod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
